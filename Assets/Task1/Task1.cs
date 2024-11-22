7dc821bd2d4f84586957b86f6f4a2a25b209812d
TaniCorn <63819551+TaniCorn@users.noreply.github.com>
2024-11-22 10:59:48 +0000

    task-task1 cleanup of final solution
    Tidied up the code for the final solution after fixing.

--- Assets/Task1/Task1.cs
+++ Assets/Task1/Task1.cs
@@ -2,47 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Task1 : MonoBehaviour
+public class Task1
 {
-
-    public void Start()
-    {
-        float gravity = -9.8f;
-        float xPosition = 0;
-        //Testing if it can reach height
-        bool success = TryCalculateXPositionAtHeight(10.0f, new Vector2(3, 6), new Vector2(0, 2), gravity, 5, ref xPosition);
-        Debug.Log("Test 1H: " +  success + " with xPosition: " + xPosition);
-        
-        //Testing left movement
-        success = TryCalculateXPositionAtHeight(5.0f, new Vector2(3, 6), new Vector2(-1, 0), gravity, 5, ref xPosition);
-        Debug.Log("Test 2LM: " +  success + " with xPosition: " + xPosition);
-
-        //Testing right movement
-        success = TryCalculateXPositionAtHeight(5.0f, new Vector2(3, 6), new Vector2(1, 0), gravity, 5, ref xPosition);
-        Debug.Log("Test 3RM: " + success + " with xPosition: " + xPosition);
-
-        //Testing left bounce
-        success = TryCalculateXPositionAtHeight(5.0f, new Vector2(0.1f, 6), new Vector2(-1, 0), gravity, 5, ref xPosition);
-        Debug.Log("Test 4LB: " + success + " with xPosition: " + xPosition);
-
-        //Testing right bounce
-        success = TryCalculateXPositionAtHeight(5.0f, new Vector2(4.9f, 6), new Vector2(1, 0), gravity, 5, ref xPosition);
-        Debug.Log("Test 5RB: " + success + " with xPosition: " + xPosition);
-
-        //Testing right bounce extreme
-        success = TryCalculateXPositionAtHeight(5.0f, new Vector2(0.5f, 6), new Vector2(2, 0), gravity, 1, ref xPosition);
-        Debug.Log("Test 6RBE: " + success + " with xPosition: " + xPosition);
-
-        //Testing left bounce extreme
-        success = TryCalculateXPositionAtHeight(5.0f, new Vector2(0.5f, 6), new Vector2(-2, 0), gravity, 1, ref xPosition);
-        Debug.Log("Test 7LBE: " + success + " with xPosition: " + xPosition);
-    }
-
 
     bool TryCalculateXPositionAtHeight(float h, Vector2 p, Vector2 v, float G, float w, ref float xPosition)
     {
         //See ReadME, G Issue. In short, no assumption is made that G will be negative.
-        if(G > 0)
+        if (G > 0)
         {
             G *= -1;
         }
@@ -53,7 +19,7 @@ public class Task1 : MonoBehaviour
         Vector2 horizontal = Vector2.right;
         float angle = Vector2.Angle(horizontal, v);
 
-        float maximumHeight = ( (v.y * v.y) * (Mathf.Sin(angle) * Mathf.Sin(angle)) ) / 2 * G;
+        float maximumHeight = ((v.y * v.y) * (Mathf.Sin(angle) * Mathf.Sin(angle))) / 2 * G;
 
         // If initial position is higher than 'h' already, or initial position and max height is higher than 'h',
         // continue calculations, otherwise return false
@@ -62,52 +28,32 @@ public class Task1 : MonoBehaviour
             return false;
         }
 
-        // s = u*t + 1/2 * a * t^2
-        // Re-arranged formula for time
-        float displacement = h - p.y; // Imagining intial point now exists at y=0 and h gets shifted down the same amount
-        float t = (-v.y - Mathf.Sqrt(v.y*v.y + 2*G*displacement)) / G; // Get the time we hit h
+        // Imagining intial point now exists at y=0 and h gets shifted down the same amount
+        float yDisplacement = h - p.y;
+        // s = u*t + 1/2 * a * t^2, Re-arranged this formula for time
+        // Get the time the ball reaches height h
+        float t = (-v.y - Mathf.Sqrt(v.y * v.y + 2 * G * yDisplacement)) / G; 
 
-        // How far along the x-axis have we moved when time = t
-        // s = u*t
-        float xDisplacement = v.x * t;
+        // s = u*t, How far along the x-axis have we moved when time = t
+        // Displacement relative to x = 0
+        float xDisplacement = (v.x * t) + p.x;
 
         // Determining the amount of times we've bounced and which wall we bounce on
-        float overallDisplacement = xDisplacement + p.x;
-        int bounces = Mathf.FloorToInt(overallDisplacement / w); // TODO: problem with negative bounces
+        // bounces < 0 then the left wall was the first wall, bounces > 0 then the right wall was the first wall
+        int bounces = Mathf.FloorToInt(xDisplacement / w);
         if (bounces == 0)
         {
-            xPosition = xDisplacement + p.x;
+            xPosition = xDisplacement;
             return true;
         }
 
-        float scaledDisplacement = (w * bounces - overallDisplacement);
+        // Bounce on right wall 5 - 5.4 = -0.4
+        // Bounce on left wall -5 - -0.4 = -4.6
+        // Getting the displacement relative to x = w, should always be negative and less than w
+        float scaledDisplacement = (w * bounces - xDisplacement);
 
         xPosition = w + scaledDisplacement;
         return true;
 
-        int signBounces = Mathf.Abs(bounces);
-
-        //If v.x > 0 then we must bounce on the right wall first.
-        //If v.x < 0 then we bounce on the left wall first. Must flip the reflection sign.
-        //Flip reflection
-        if (v.x < 0)
-        {
-            signBounces++;
-        }
-        int reflectionSign = signBounces % 2;
-
-        // If we have a reflection sign of 0, we bounced off the left wall
-        // If we have a reflection sign of 1, we bounced off the right wall
-        if (reflectionSign == 0)
-        {
-            xPosition = scaledDisplacement;
-        }
-        else
-        {
-            xPosition = w - scaledDisplacement;
-        }
-
-        return true;
     }
-
 }
