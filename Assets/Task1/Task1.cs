d0ff02722630d36e1191f4e8c60e03435ff6d92c
TaniCorn <63819551+TaniCorn@users.noreply.github.com>
2024-11-22 08:55:06 +0000

    dev-task1 xPosition calculations
    Calculated xPosition using time we hit the specified h and distance travelled.

--- Assets/Task1/Task1.cs
+++ Assets/Task1/Task1.cs
@@ -20,14 +20,33 @@ public class Task1
 
         float maximumHeight = ( (v.y * v.y) * (Mathf.Sin(angle) * Mathf.Sin(angle)) ) / 2 * G;
 
-        //If initial position is higher than 'h' already, or initial position and max height is higher than 'h', continue calculations
+        // If initial position is higher than 'h' already, or initial position and max height is higher than 'h',
+        // continue calculations, otherwise return false
         if (!(p.y > h || p.y + maximumHeight > h))
         {
             return false;
         }
 
+        // s = u*t + 1/2 * a * t^2
+        // Re-arranged formula for time
+        float displacement = h - p.y; // Imagining point now exists at y=0 and h gets shifted down the same amount
+        float t = (-v.y + Mathf.Sqrt(v.y*v.y + 2*G*displacement)) / G;
 
-
+        // How far along the x-axis have we moved when time = t
+        // s = u*t
+        float xDisplacement = v.x * t;
+        int boxesTravelled = Mathf.FloorToInt((xDisplacement + p.x / w));
+        float finalBounceDistance = xDisplacement - w*boxesTravelled;
+        int reflectionSign = boxesTravelled % 2;
+        //If we have a reflection sign of 1, then we are in the reflected space (We have bounced)
+        if (reflectionSign == 0)
+        {
+            xPosition = finalBounceDistance;
+        }
+        else
+        {
+            xPosition = w - finalBounceDistance;
+        }
         return true;
     }
 
