9b3a067914e9e4fad962b7ceb66efdadbc0b96fa
TaniCorn <63819551+TaniCorn@users.noreply.github.com>
2024-11-22 08:12:27 +0000

    fix-task1 G issue
    G is not assumed negative, so we ensure it is negative.

--- Assets/Task1/Task1.cs
+++ Assets/Task1/Task1.cs
@@ -6,6 +6,11 @@ public class Task1
 {
     bool TryCalculateXPositionAtHeight(float h, Vector2 p, Vector2 v, float G, float w, ref float xPosition)
     {
+        //See ReadME, G Issue. In short, no assumption is made that G will be negative.
+        if(G > 0)
+        {
+            G *= -1;
+        }
 
         // Greatest Height Attained formula
         // V^2 * Sin^2 * a / 2g , where 'a' is the angle between the horizontal axis
@@ -15,15 +20,15 @@ public class Task1
 
         float maximumHeight = ( (v.y * v.y) * (Mathf.Sin(angle) * Mathf.Sin(angle)) ) / 2 * G;
 
-        //If initial position is higher than 'h' already, or initial position and max height is higher than 'h'
-        if (p.y > h || p.y + maximumHeight > h)
-        {
-            return true;
-        }
-        else
+        //If initial position is higher than 'h' already, or initial position and max height is higher than 'h', continue calculations
+        if (!(p.y > h || p.y + maximumHeight > h))
         {
             return false;
         }
+
+
+
+        return true;
     }
 
 }
